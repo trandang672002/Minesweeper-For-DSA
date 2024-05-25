91c17dcd0ccbe7049f737c5c1fad55c8af8782a1
trandang672002 <117880035+trandang672002@users.noreply.github.com>
2024-05-25 22:06:25 +0700

    update file

---
+++ src/StartWindow.java
@@ -0,0 +1,12 @@
+package src;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class StartWindow extends JFrame {
+    public StartWindow(){
+        setSize(600,600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
